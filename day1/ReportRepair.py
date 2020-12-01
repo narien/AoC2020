2915ef15b642430ac334a6c10d235916b480e993
Jakob Svemar <bgm@qlik.com>
2020-12-01 11:11:08 +0100

    improve day1 solution using combinations and reduce

--- day1/ReportRepair.py
+++ day1/ReportRepair.py
@@ -1,24 +1,16 @@
+from itertools import combinations
+from functools import reduce
 
-def calcPartOne (expenses):
-    for i in range(len(expenses)):
-        for j in range (len(expenses)):
-            if i != j:
-                if expenses[i] + expenses[j] == 2020:
-                    print('Part 1 product is: ' + str(expenses[i] * expenses[j]))
-                    return
-
-def calcPartTwo (expenses):
-    for i in range(len(expenses)):
-        for j in range (len(expenses)):
-            for k in range (len(expenses)):
-                if i != j and i != k and j != k:
-                    if expenses[i] + expenses[j] + expenses[k] == 2020:
-                        print('Part 2 product is: ' + str(expenses[i] * expenses[j] * expenses[k]))
-                        return
+def calc2020Product(expenses, nbrOfexpenses):
+    for comb in combinations(expenses, nbrOfexpenses):
+        if sum(comb) == 2020:
+            return (reduce((lambda x, y: x * y), comb))
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         expenses = [int(val) for val in f]
-    calcPartOne(expenses)
-    calcPartTwo(expenses)
+
+    print(calc2020Product(expenses, 2))
+    print(calc2020Product(expenses, 3))
+
 
