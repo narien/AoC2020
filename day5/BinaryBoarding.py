d7cf07b2d7dce3767e163fd99315942e380af3f9
Jakob Svemar <bgm@qlik.com>
2020-12-08 13:45:56 +0100

    clean up day5 solution

--- day5/BinaryBoarding.py
+++ day5/BinaryBoarding.py
@@ -1,9 +1,9 @@
 
 if __name__ == '__main__':
     with open('input.txt') as f:
-        input = [line.strip() for line in f]
+        pattern = str.maketrans("FBLR", "0101")
+        intInput = [int(line.strip().translate(pattern), 2) for line in f]
 
-    intInput = [int(line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2) for line in input]
     intInput.sort()
     print('Highest seat ID: ', intInput[-1])
 
